a291b87c84d95de41432be7d78bf06d7679923ce
Markus <markus.myhle@live.com>
2017-04-26 12:51:39 +0300

    Add Active Record connection management middleware
    Extracted from engagement-registry

---
+++ lib/salestation/web/active_record_connection_management.rb
@@ -0,0 +1,22 @@
+module Salestation
+  class Web < Module
+    class ActiveRecordConnectionManagement
+      def initialize(app)
+        @app = app
+      end
+
+      def call(env)
+        testing = env['rack.test']
+
+        status, headers, body = @app.call(env)
+        proxy = ::Rack::BodyProxy.new(body) do
+          ActiveRecord::Base.clear_active_connections! unless testing
+        end
+        [status, headers, proxy]
+      rescue Exception
+        ActiveRecord::Base.clear_active_connections! unless testing
+        raise
+      end
+    end
+  end
+end
