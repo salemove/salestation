5a5702ceaf749db54e66c88a55fc18c0b956d47d
Indrek Juhkam <indrek@urgas.eu>
2017-09-01 13:13:04 +0300

    Allow returning arrays as responses
    This is useful when an endpoint returns a list of objects.

--- spec/salestation/web/responses/success_spec.rb
+++ spec/salestation/web/responses/success_spec.rb
@@ -5,17 +5,34 @@ describe Salestation::Web::Responses::Success do
 
   let(:attributes) { {status: status, body: body} }
   let(:status) { 200 }
-  let(:body) { {key: 'value'} }
 
-  it 'has status' do
-    expect(create_success.status).to eq(status)
+  context 'when body is a hash' do
+    let(:body) { {key: 'value'} }
+
+    it 'has status' do
+      expect(create_success.status).to eq(status)
+    end
+
+    it 'has body' do
+      expect(create_success.body).to eq(body)
+    end
   end
 
-  it 'has body' do
-    expect(create_success.body).to eq(body)
+  context 'when body is an array' do
+    let(:body) { [{foo: 'bar'}, {foo: 'baz'}] }
+
+    it 'has status' do
+      expect(create_success.status).to eq(status)
+    end
+
+    it 'has body' do
+      expect(create_success.body).to eq(body)
+    end
   end
 
   describe '.with_code' do
+    let(:body) { {key: 'value'} }
+
     it 'creates success with provided code' do
       expect(described_class.with_code(201).new(attributes).status)
         .to eq(201)
