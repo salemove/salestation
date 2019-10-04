ba4288f885ce0228e83c84d64f2716ea0dbe938c
Riivo Parts <riivo.parts@salemove.com>
2019-10-04 15:11:38 +0300

    Add optional debug_message for all Errors
    These values will be used in FE to determine correct error to display.

--- spec/salestation/app/errors/dependency_currently_unavailable_spec.rb
+++ spec/salestation/app/errors/dependency_currently_unavailable_spec.rb
@@ -8,4 +8,16 @@ describe Salestation::App::Errors::DependencyCurrentlyUnavailable do
   it 'has message' do
     expect(create_dependency_currently_unavailable.message).to eq(message)
   end
+
+  context 'when debug_message is provided' do
+    subject(:create_dependency_currently_unavailable) do
+      described_class.new(message: message, debug_message: debug_message)
+    end
+
+    let(:debug_message) { 'something' }
+
+    it 'creates getter for debug_message' do
+      expect(create_dependency_currently_unavailable.debug_message).to eq('something')
+    end
+  end
 end
