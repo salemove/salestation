1dcbe187aca182a460041b53a70eb35b70d796c0
urmastalimaa <urmas7@gmail.com>
2020-03-09 13:43:22 +0200

    Allow per-request tags for statsd
    Helps to differentiate kinds of requests in metrics (reporting,
    read-only, maintenance, etc).

--- lib/salestation/web/statsd_middleware.rb
+++ lib/salestation/web/statsd_middleware.rb
@@ -3,6 +3,7 @@
 module Salestation
   class Web < Module
     class StatsdMiddleware
+      EXTRA_TAGS_ENV_KEY = 'salestation.statsd.tags'
 
       def initialize(app, statsd, metric:)
         @app = app
@@ -23,11 +24,13 @@ module Salestation
             'unknown-route'
           end
 
-        @statsd.timing(@metric, (Time.now - start) * 1000, tags: [
+        tags = [
           "path:#{ path }",
           "method:#{ method }",
           "status:#{ status }"
-        ])
+        ] + env.fetch(EXTRA_TAGS_ENV_KEY, [])
+
+        @statsd.timing(@metric, (Time.now - start) * 1000, tags: tags)
 
         [status, header, body]
       end
