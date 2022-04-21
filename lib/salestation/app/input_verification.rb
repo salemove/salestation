9097ccb852d786e4eca1ea6cc213cb391110c7cb
Brandon Autrey <brandon.autrey@glia.com>
2022-04-21 15:21:30 +0300

    Change input verification to new dry-validation format
    In dry-validation >v1, the result object has changed. The result
    does not have an `output` method anymore. Instead it needs to
    be converted into a hash. Same with the errors and hints.
    
    Added backwards compatibility for when an older version of
    dry-validation is being used.
    
    MSG-75
    MSG-71

--- lib/salestation/app/input_verification.rb
+++ lib/salestation/app/input_verification.rb
@@ -6,12 +6,27 @@ module Salestation
       def verify_input(schema)
         -> (request) do
           result = schema.call(request.input)
-          if result.success?
-            request.replace_input(result.output)
+
+          dry_validation_version = Gem.loaded_specs['dry-validation'].version
+          if dry_validation_version < Gem::Version.new('1.0')
+            Mapper.from_dry_validation_result(result.output, result.errors, custom_error_map)
+            if result.success?
+              request.replace_input(result.output)
+            else
+              Deterministic::Result::Failure(
+                Errors::InvalidInput.new(errors: result.errors, hints: result.hints)
+              )
+            end
+          elsif dry_validation_version <= Gem::Version.new('1.8')
+            if result.success?
+              request.replace_input(result.to_h)
+            else
+              Deterministic::Result::Failure(
+                Errors::InvalidInput.new(errors: result.errors.to_h, hints: result.hints.to_h)
+              )
+            end
           else
-            Deterministic::Result::Failure(
-              Errors::InvalidInput.new(errors: result.errors, hints: result.hints)
-            )
+            raise 'Unsupported dry-validation version'
           end
         end
       end
